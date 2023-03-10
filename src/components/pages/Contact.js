81be1376e6c4e7f43f79d7e124ccaebe124c53a5
jscobie <112592660+jscobie@users.noreply.github.com>
2023-03-09 20:53:13 -0500

    enabled contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -11,17 +11,17 @@ export default function Contact() {
         <p style={{"marginBottom":"30%"}}><form style={{"display":"block","textAlign":"left","width":"300px","marginTop":"8px"}}>
         <label style={{"display":"block","textAlign":"left","width":"300px","marginTop":"8px"}}>
         Name:
-        <input style={{"display":"block","textAlign":"left","width":"300px","marginTop":"8px"}} type="text" name="name" disabled />
+        <input style={{"display":"block","textAlign":"left","width":"300px","marginTop":"8px"}} type="text" name="name" />
         </label>
         <label style={{"display":"block","textAlign":"left","width":"300px","marginTop":"8px"}}>
         Email:
-        <input style={{"display":"block","textAlign":"left","width":"300px","marginTop":"8px"}} type="email" name="email" disabled />
+        <input style={{"display":"block","textAlign":"left","width":"300px","marginTop":"8px"}} type="email" name="email" />
         </label>
         <label style={{"display":"block","textAlign":"left","width":"300px","marginTop":"8px"}}>
         Message:
-        <input style={{"display":"block","textAlign":"left","width":"300px","marginTop":"8px"}} type="text" name="message" disabled />
+        <input style={{"display":"block","textAlign":"left","width":"300px","marginTop":"8px"}} type="text" name="message" />
         </label>
-        <input style={{ "marginTop": '20px', "backgroundColor":"#42c5f5","color":"white","border":"none","borderRadius":"4px","padding":"2px 20px 2px","whiteSpace":"nowrap" }} type="submit" value="Submit" disabled />
+        <input style={{ "marginTop": '20px', "backgroundColor":"#42c5f5","color":"white","border":"none","borderRadius":"4px","padding":"2px 20px 2px","whiteSpace":"nowrap" }} type="submit" value="Submit" />
         </form>
         </p>
       </div>
